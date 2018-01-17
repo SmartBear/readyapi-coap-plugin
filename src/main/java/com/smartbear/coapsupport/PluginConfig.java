1fd5b350bf0714ad31fdd4a2e3a94784b52c37a2
Pavel.Shelentsov <pavel.shelentsov@smartbear.com>
2018-01-17 11:39:15 +0300

    RIA-4725 Update version to 1.0.1

--- src/main/java/com/smartbear/coapsupport/PluginConfig.java
+++ src/main/java/com/smartbear/coapsupport/PluginConfig.java
@@ -5,25 +5,23 @@ import com.eviware.soapui.impl.wsdl.submit.RequestTransportRegistry;
 import com.eviware.soapui.plugins.PluginAdapter;
 import com.eviware.soapui.plugins.PluginConfiguration;
 import com.eviware.soapui.support.UISupport;
-import com.smartbear.coapsupport.CoapRequestTestStepActionGroup;
 import org.eclipse.californium.core.network.config.NetworkConfig;
 
-@PluginConfiguration(groupId = "com.smartbear.plugins", name = "CoAP Support Plugin", version = "1.0.0",
+@PluginConfiguration(groupId = "com.smartbear.plugins", name = "CoAP Support Plugin", version = "1.0.1",
         autoDetect = true, description = "Adds CoAP Request test step to SoapUI NG",
-        infoUrl = "")
+        infoUrl = "", minimumReadyApiVersion = "2.2.0")
 public class PluginConfig extends PluginAdapter {
 
     final static String COAP_LOG = "coap.log";
     private boolean shouldLoad;
 
 
-    public PluginConfig(){
+    public PluginConfig() {
         super();
-        try{
+        try {
             Class.forName("com.smartbear.ready.ui.ReadyApiMain");
             shouldLoad = true;
-        }
-        catch (Throwable e){
+        } catch (Throwable e) {
             shouldLoad = false;
         }
         UISupport.addResourceClassLoader(getClass().getClassLoader());
@@ -44,8 +42,7 @@ public class PluginConfig extends PluginAdapter {
             if (RequestTransportRegistry.getTransport("coap") == null) {
                 RequestTransportRegistry.addTransport("coap", new CoapTransport());
             }
-        }
-        catch(RequestTransportRegistry.MissingTransportException e){
+        } catch (RequestTransportRegistry.MissingTransportException e) {
             RequestTransportRegistry.addTransport("coap", new CoapTransport());
         }
     }
